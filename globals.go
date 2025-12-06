483701e3b765746962efa8ed9a04bd18c20623bd
Sighery <11218602+Sighery@users.noreply.github.com>
2025-12-06 17:39:08 +0100

    Add private globals
    This is mainly used with the C callbacks to pass messages around.

---
+++ globals.go
@@ -0,0 +1,25 @@
+package gokindlebt
+
+/*
+#include <kindlebt/kindlebt.h>
+*/
+import "C"
+
+import (
+	"sync"
+	"time"
+)
+
+const (
+	callbackTimeout = 10 * time.Second
+)
+
+var (
+	// Need this to convert to BleConnection later on in callbacks
+	connRegistry = make(map[C.bleConnHandle]*BleConnection)
+
+	// Registry for callback operations
+	pendingReads         sync.Map
+	pendingWrites        sync.Map
+	pendingNotifications sync.Map
+)
