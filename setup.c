c05a7685fc5421a91d899efaafc9f0605ea0eec0
Sighery <11218602+Sighery@users.noreply.github.com>
2025-12-06 17:27:30 +0100

    Add C globals setup code
    For now I'm just hardcoding a single session, BLE connection, and one
    GATT Service in the C side, that I'll re-use on the Go side. This is not
    ideal because it means you can only have a single
    session/connection/service going on at the same time, but for now it's
    good enough.

---
+++ setup.c
@@ -0,0 +1,5 @@
+#include "setup.h"
+
+sessionHandle gokindlebt_session = NULL;
+bleConnHandle gokindlebt_conn = NULL;
+bleGattsService_t gokindlebt_gatt_db;
---
+++ setup.h
@@ -0,0 +1,10 @@
+#ifndef GOKINDLEBT_SETUP_H
+#define GOKINDLEBT_SETUP_H
+
+#include <kindlebt/kindlebt.h>
+
+extern sessionHandle gokindlebt_session;
+extern bleConnHandle gokindlebt_conn;
+extern bleGattsService_t gokindlebt_gatt_db;
+
+#endif // GOKINDLEBT_SETUP_H
